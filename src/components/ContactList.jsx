cfef0940f5521655d1fedc67d89b398ee5bfd46f
Warllen <warlenromero14@gmail.com>
2023-11-06 19:09:05 -0500

    first commit

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactDelete from './ContactDelete';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [];
+  });
+
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
+  const handleDeleteContact = (id) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
+    ContactDelete(id);
+  };
+
   return (
-    <ul>
-      {contacts &&
-        contacts.map((contact) => (
-          <ContactDelete key={contact.id} contact={contact} onDeleteContact={onDeleteContact} />
+    <div>
+      <h2>Contact List</h2>
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} - {contact.phone}
+            <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
+          </li>
         ))}
-    </ul>
+      </ul>
+    </div>
   );
 };
 
 export default ContactList;
+
